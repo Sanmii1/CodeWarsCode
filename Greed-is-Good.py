d708547bc514def4f5da0487f7a45740a28e9ee0
Sanmi <145213955+Sanmii1@users.noreply.github.com>
2024-12-03 18:06:51 +0700

    Update Greed-is-Good.py

--- Greed-is-Good.py
+++ Greed-is-Good.py
@@ -1,12 +1,15 @@
 def score(dice):
     point = 0
     numberCollections = {}
+
+    # Mencari Jumlah nomor yang sama ada berapa,disimpan dalam bentuk dictionary
     for num in dice:
         if numberCollections.get(num,0) >= 1:
             numberCollections[num] = numberCollections.get(num) + 1
         else :
             numberCollections[num] = numberCollections.get(num,0) + 1
-
+            
+    # mencari nomor yang jumlahnya lebih dari atau sama dengan 3 
     for key in numberCollections :
         if numberCollections[key] >= 3 :
             if key == 1 :
@@ -15,7 +18,8 @@ def score(dice):
             else :
                 point += key * 100
                 numberCollections[key] = numberCollections.get(key) - 3
-    
+                
+    # mengecek kembali apakah masih tersisa bilangan 1 dan 5
     for key in numberCollections :
         if key == 1 :
             point += 100 * numberCollections[key]
